ffcce878210c124939d23bd5d82194a7f38f09f1
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2022-10-27 16:36:21 +0530

    Update airline.sql

--- Database/airline.sql
+++ Database/airline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2020 at 05:10 PM
+-- Generation Time: Oct 27, 2022 at 10:10 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.1.12
 
